a67e2db8ee9ce8c6cb10a89f58795c3a29c48b91
Saikaisiham <blackbutlersiham2001@gmail.com>
2023-12-25 17:01:50 +0100

    commit

--- Array_based_list.py
+++ Array_based_list.py
@@ -24,7 +24,6 @@ class ArrayListType:
         remove(self, item): Removes the first occurrence of the given item from the array.
         remove_at(self, loc): Removes the item at the specified location from the array.
     """
-
     def __init__(self, size):
         if size > 0:
             self.size = size
@@ -33,17 +32,114 @@ class ArrayListType:
         else:
             print("Size must be greater than 0.")
 
-    # ... (rest of the class methods)
 
-def main():
-    """
-    Main function to demonstrate the usage of the ArrayListType class.
-    """
+    def __del__(self):
+        del self.list
+
+    def isEmpty(self):
+        return self.length == 0
+    
+    def isFull(self):
+        return self.length == self.size
+    
+    def list_size(self):
+        return self.length
+    
+    def print_list(self):
+        for i in range(self.length): 
+            print(i)
+        print()
+
+    def is_item_at_equal(self, loc, item):
+        if 0 <= loc < self.length:
+            return self.list[loc] == item
+        return False
+        
+
+    def insert_at(self, loc, item):
+        if self.isFull(): 
+            print('List is Full')
+        if loc < 0 or loc > self.length: 
+            print('Out of range') 
+
+        else: 
+            self.list.insert(loc, item)
+            self.length += 1
+
+
+    def insert_end(self, item): 
+        if self.isFull(): 
+            print('List is Full')
+        else:
+            self.list[self.length] = item
+            self.length += 1
+
+
+    def retrieve_at(self, loc): 
+        if 0 <= loc < self.length:
+            return self.list[loc]
+        else:
+            print("Out of Range")
+
+
+
+    def replace_at(self, loc, item): 
+        if 0 <= loc < self.length:
+            self.list[loc] = item
+        else:
+            print("Out of Range")
+            
+
+
+    def search(self, item):
+        for loc in range(self.length):
+            if self.list[loc] == item:
+                return loc
+        return -1
+    
+
+    def avoid_duplicate(self, item): 
+        if self.isFull():
+            print('The List is Full')
+
+        else: 
+            search = self.search(item)
+            if search == -1: 
+                self.list[self.length] = item
+                self.length += 1
+            else:
+                print("No duplicates are allowed")
+
+
+
+
+    def remove(self, item):
+        loc = self.search(item)
+        if loc == -1:
+            print("The item to be deleted is not in the list")
+        else:
+            self.remove_at(loc)
+
+
+
+    def remove_at(self, loc):
+        if 0 <= loc < self.length:
+            del self.list[loc]
+            self.length -= 1
+        else:
+            print("The location of the item to be removed is out of range")
+
+
+
+
+def main(): 
     ls = ArrayListType(50)
-    for i in range(50):
-        ls.insert_at(i, i)
+    for i in range(50): 
+        ls.insert_at(i,i)
+
 
     ls.print_list()
 
-if __name__ == '__main__':
+
+if __name__ == '__main__': 
     main()
--- Stack-impl.py
+++ Stack-impl.py
@@ -1,57 +1,105 @@
 class Stack:
+    """
+    Stack class represents a simple stack data structure.
+
+    Attributes:
+        array_size (int): The maximum size of the stack.
+        top (int): Index of the top element in the stack (-1 for an empty stack).
+        item (list): List to store elements in the stack.
+
+    Methods:
+        __init__(self, array_size): Initializes a new Stack with the specified array size.
+        is_empty(self): Checks if the stack is empty.
+        push(self, element): Pushes an element onto the stack.
+        pop(self): Pops the top element off the stack without returning it.
+        pop_element(self): Pops and returns the top element from the stack.
+        get_top(self): Returns the top element of the stack without removing it.
+        print_stack(self): Prints the elements of the stack.
+    """
+
     def __init__(self, array_size):
+        """
+        Initializes a new Stack with the specified array size.
+
+        Parameters:
+            array_size (int): The maximum size of the stack.
+        """
         self.array_size = array_size
         self.top = -1
         self.item = [None] * self.array_size
 
-
     def is_empty(self):
+        """
+        Checks if the stack is empty.
+
+        Returns:
+            bool: True if the stack is empty, False otherwise.
+        """
         return self.top < 0
-    
+
     def push(self, element):
+        """
+        Pushes an element onto the stack.
+
+        Parameters:
+            element: The element to be pushed onto the stack.
+
+        Returns:
+            str: 'Stack full on push' if the stack is full, otherwise None.
+        """
         if self.top >= self.array_size - 1:
             return ' Stack full on push'
-        
-
-        else :
+        else:
             self.top += 1
             self.item[self.top] = element
 
-
     def pop(self):
+        """
+        Pops the top element off the stack without returning it.
+
+        Returns:
+            str: 'Stack empty on pop' if the stack is empty, otherwise None.
+        """
         if self.is_empty():
             return 'Stack empty on pop'
-        
-        else : 
+        else:
             self.top -= 1
 
-
     def pop_element(self):
+        """
+        Pops and returns the top element from the stack.
+
+        Returns:
+            str: 'Stack empty on pop' if the stack is empty, the popped element otherwise.
+        """
         if self.is_empty():
             return 'Stack empty on pop'
-        
-        else: 
+        else:
             element = self.item[self.top]
             self.top -= 1
             return element
-        
-
 
     def get_top(self):
+        """
+        Returns the top element of the stack without removing it.
+
+        Returns:
+            str: 'Stack empty on pop' if the stack is empty, the top element otherwise.
+        """
         if self.is_empty():
             return 'Stack empty on pop'
-        
-        else: 
-            Stack_top = self.item[self.top]
-            return Stack_top
-        
+        else:
+            stack_top = self.item[self.top]
+            return stack_top
 
     def print_stack(self):
+        """
+        Prints the elements of the stack.
+        """
         print("[", end=" ")
         for i in range(self.top, -1, -1):
             print(self.item[i], end=" ")
         print("]")
-
 
 
 
